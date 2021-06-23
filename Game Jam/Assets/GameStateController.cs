94bbe92258dd595d18b434238ef2f8523f6c5276
Len Farag <glizzyzbro@hotmail.com>
2021-06-24 09:52:40 +1000

    Added Orthogonal Test Scenes
    For both Len and Thomas

---
+++ Game Jam/Assets/GameStateController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class GameStateController : MonoBehaviour
+{
+
+#region Public Fields, Statics, & Constants
+#endregion
+
+#region Private Methods
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+#endregion
+
+#region Public & Protected Methods
+#endregion
+
+#region Private Fields, Statics, & Constants
+#endregion
+
+}
