3a552f1245d8819cb4b1e584935f1bfada1b422e
Len Farag <glizzyzbro@hotmail.com>
2021-06-24 10:07:17 +1000

    Added IInteractor IInteractable

---
+++ Game Jam/Assets/Scripts/IInteractable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+    void OnInteract( IInteractor a_Interactor );
+    void OnUninteract( IInteractor a_Interactor )
+}
