7b1af781200ff0741701742e1321746c585aa1b0
Len Farag <glizzyzbro@hotmail.com>
2021-06-24 10:37:37 +1000

    Added ISingleton

---
+++ Game Jam/Assets/Scripts/ISingleton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ISingleton< T > : MonoBehaviour where T : ISingleton< T >
+{
+    public static T Instance
+    {
+        get
+        {
+            if ( m_Instance == null )
+            {
+                m_Instance = FindObjectOfType< T >();
+
+                if ( m_Instance == null )
+                {
+                    Debug.LogError( "No object of type " + m_Instance.GetType().Name + " could be found!" );
+                }
+            }
+
+            return m_Instance;
+        }
+    }
+
+    private static T m_Instance;
+}
