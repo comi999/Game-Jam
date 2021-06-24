086e1ed180c1f24271e9997fedf2a9d08e6ae18f
Suqnier <sl00ka@hotmail.com>
2021-06-24 16:06:36 +1000

    Spinks - prefab updates

---
+++ Game Jam/Assets/Scripts/Environment/RandomRotation.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    private void Awake() => gameObject.transform.localRotation = Quaternion.Euler(0f, Random.Range(0, 360), 0f);
+}
