aa3c2eaf92bd70a7fca988f1c682c33edd75760f
Len Farag <glizzyzbro@hotmail.com>
2021-06-25 14:39:57 +1000

    Added buttons and hover enlargement

---
+++ Game Jam/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MenuButton : Button
+{
+    protected override void Awake()
+    {
+        m_Image = gameObject.GetComponent< Image >();
+        m_Rect = gameObject.GetComponent< RectTransform >();
+        m_OriginalSize = m_Rect.sizeDelta;
+    }
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        base.OnPointerEnter(eventData);
+        m_Rect.sizeDelta = m_OriginalSize * 1.3f;
+    }
+
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        base.OnPointerUp(eventData);
+        m_Rect.sizeDelta = m_OriginalSize;
+    }
+
+    private Image m_Image;
+    private RectTransform m_Rect;
+    private Vector2 m_OriginalSize;
+}
--- Game Jam/Assets/Scripts/SubUIController.cs
+++ Game Jam/Assets/Scripts/SubUIController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SubUIController : MonoBehaviour
 {
@@ -11,4 +12,6 @@ public class SubUIController : MonoBehaviour
     public void PauseMenu_OnExit() => UIController.Instance.PauseMenu_OnExit();
     public void ScoreMenu_OnQuit() => UIController.Instance.ScoreMenu_OnQuit();
     public void ScoreMenu_OnExit() => UIController.Instance.ScoreMenu_OnExit();
+
+    
 }
