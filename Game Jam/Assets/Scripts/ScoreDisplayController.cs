0911c14ac524e260d75b06c54cb9f1e62e980344
Len Farag <glizzyzbro@hotmail.com>
2021-06-25 15:45:00 +1000

    Resync

--- Game Jam/Assets/Scripts/ScoreDisplayController.cs
+++ Game Jam/Assets/Scripts/ScoreDisplayController.cs
@@ -5,11 +5,28 @@ using UnityEngine.UI;
 
 public class ScoreDisplayController : MonoBehaviour
 {
-    public Text ScoreText;
-    public Sprite Background;
+    public Image Left;
+    public Image Middle;
+    public Image Right;
+
+    public Sprite[] Numbers;
 
     private void Start()
     {
-        ScoreText.text = "SCORE: " + UIController.Instance.Score.ToString();
+        PushScore();
+    }
+
+    public void PushScore()
+    {
+        int[] digits = UIController.Instance.GetScoreDigits();
+
+        Left.sprite = Numbers[ digits[ 2 ] ];
+        Middle.sprite = Numbers[ digits[ 1 ] ];
+        Right.sprite = Numbers[ digits[ 0 ] ];
+
+        Color tint = new Color(1, 1, 1, 1);
+        Left.color = tint;
+        Middle.color = tint;
+        Right.color = tint;
     }
 }
--- Game Jam/Assets/Scripts/UIController.cs
+++ Game Jam/Assets/Scripts/UIController.cs
@@ -10,12 +10,25 @@ public class UIController : ISingleton< UIController >
     public string PauseMenu = "PauseMenu";
     public string ScoreMenu = "ScoreMenu";
 
+    public ScoreDisplayController ScoreController;
+
     public KeyCode PauseMenuKey;
 
     public bool IsMainMenuLoaded { get; private set; }
     public bool IsPauseMenuLoaded { get; private set; }
     public bool IsScoreMenuLoaded { get; private set; }
-    public int Score { get; set; }
+    public int Score
+    {
+        get
+        {
+            return m_Score;
+        }
+        set
+        {
+            m_Score = value;
+            ScoreController.PushScore();
+        }
+    }
 
     public AudioPool[] AllAudioPools;
 
@@ -38,11 +51,15 @@ public class UIController : ISingleton< UIController >
             {
                 SceneManager.UnloadSceneAsync( PauseMenu ).completed += a => IsPauseMenuLoaded = false;
                 PointerController.Instance.IsActive = true;
+                ScoreController.gameObject.SetActive( true );
+                Time.timeScale = 1;
             }
             else
             {
                 SceneManager.LoadSceneAsync( PauseMenu, LoadSceneMode.Additive ).completed += a => IsPauseMenuLoaded = true;
                 PointerController.Instance.IsActive = false;
+                ScoreController.gameObject.SetActive( false );
+                Time.timeScale = 0;
             }
         }
     }
@@ -54,6 +71,7 @@ public class UIController : ISingleton< UIController >
         {
             SceneManager.LoadSceneAsync(ScoreMenu, LoadSceneMode.Additive).completed += a => IsScoreMenuLoaded = true;
             PointerController.Instance.IsActive = false;
+            ScoreController.gameObject.SetActive( false );
         }
     }
 
@@ -66,6 +84,7 @@ public class UIController : ISingleton< UIController >
         PointerController.Instance.IsActive = true;
         SoundController.Instance.Play( "BackgroundMusic", true );
         TrainManager.Instance.isDemoMode = false;
+                ScoreController.gameObject.SetActive( true );
     }
 
     public void MainMenu_OnExit()
@@ -81,7 +100,7 @@ public class UIController : ISingleton< UIController >
     {
         SceneManager.UnloadSceneAsync( PauseMenu ).completed += a => IsPauseMenuLoaded = false;
         PointerController.Instance.IsActive = true;
-
+        ScoreController.gameObject.SetActive( true );
         Time.timeScale = 1;
     }
 
@@ -89,9 +108,10 @@ public class UIController : ISingleton< UIController >
     {
         SceneManager.UnloadSceneAsync( PauseMenu ).completed += a => IsPauseMenuLoaded = false;
         SceneManager.LoadSceneAsync( MainMenu, LoadSceneMode.Additive ).completed += a => IsMainMenuLoaded = true;
+        ScoreController.gameObject.SetActive( false );
         PointerController.Instance.IsActive = false;
 
-        Time.timeScale = 0;
+        Time.timeScale = 1;
     }
 
     public void PauseMenu_OnExit()
@@ -119,4 +139,17 @@ public class UIController : ISingleton< UIController >
         Application.Quit();
         #endif
     }
+
+    public int[] GetScoreDigits()
+    {
+        int score = Score > 999 ? 999 : Score;
+        int[] digits = new int[ 3 ];
+        digits[ 0 ] = score % 10;
+        digits[ 1 ] = ( score % 100 - digits[ 0 ] ) / 10;
+        digits[ 2 ] = ( score - digits[ 0 ] - digits [ 1 ] * 10 ) / 100;
+
+        return digits;
+    }
+
+    private int m_Score;
 }
